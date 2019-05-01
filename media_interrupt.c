b08e8775ad6c1ce71d0942d450d549bed9065dc8
alwinv135 <50149720+alwinv135@users.noreply.github.com>
2019-05-01 12:44:20 -0700

    Update media_interrupt.c

--- media_interrupt.c
+++ media_interrupt.c
@@ -8,7 +8,7 @@
 /* function prototypes */
 void VGA_text (int, int, char *);
 void VGA_box (int, int, int, int, short);
-void HEX_PS2(char, char, char);
+// void HEX_PS2(char, char, char);
 
 /********************************************************************************
  * This program demonstrates use of the media ports in the DE2 Media Computer
@@ -41,22 +41,22 @@ int main(void)
 
 	/* initialize some variables */
 //	byte1 = 0; byte2 = 0; byte3 = 0; 			// used to hold PS/2 data
-	timeout = 0;										// synchronize with the timer
+//	timeout = 0;										// synchronize with the timer
 
 	/* these variables are used for a blue box and a "bouncing" ALTERA on the VGA screen */
-	int ALT_x1; int ALT_x2; int ALT_y; 
-	int ALT_inc_x; int ALT_inc_y;
-	int blue_x1; int blue_y1; int blue_x2; int blue_y2; 
+//	int ALT_x1; int ALT_x2; int ALT_y; 
+//	int ALT_inc_x; int ALT_inc_y;
+//	int blue_x1; int blue_y1; int blue_x2; int blue_y2; 
 	int screen_x; int screen_y; int char_buffer_x; int char_buffer_y;
 	short color;
 
 	/* set the interval timer period for scrolling the HEX displays */
-	int counter = 0x960000;				// 1/(50 MHz) x (0x960000) ~= 200 msec
-	*(interval_timer_ptr + 0x2) = (counter & 0xFFFF);
-	*(interval_timer_ptr + 0x3) = (counter >> 16) & 0xFFFF;
+//	int counter = 0x960000;				// 1/(50 MHz) x (0x960000) ~= 200 msec
+//	*(interval_timer_ptr + 0x2) = (counter & 0xFFFF);
+//	*(interval_timer_ptr + 0x3) = (counter >> 16) & 0xFFFF;
 
 	/* start interval timer, enable its interruts */
-	*(interval_timer_ptr + 1) = 0x7;	// STOP = 0, START = 1, CONT = 1, ITO = 1 
+//	*(interval_timer_ptr + 1) = 0x7;	// STOP = 0, START = 1, CONT = 1, ITO = 1 
 	
 	*(KEY_ptr + 2) = 0xF; 			/* write to the pushbutton interrupt mask register, and
 											 * set 3 mask bits to 1 (bit 0 is Nios II reset) */
@@ -91,45 +91,14 @@ int main(void)
 //	color = 0x187F;		// a medium blue color
 //	VGA_box (blue_x1 * 4, blue_y1 * 4, blue_x2 * 4, blue_y2 * 4, color);
 	/* output text message in the middle of the VGA monitor */
-	VGA_text (blue_x1 + 5, blue_y1 + 3, text_top_VGA);
-	VGA_text (blue_x1 + 5, blue_y1 + 4, text_bottom_VGA);
+	VGA_text (2, 1 , text_greet_VGA);
+	VGA_text (2 , 2, text_one_VGA);
+	VGA_text (2, 3, text_two_VGA);
 
-	char_buffer_x = 79; char_buffer_y = 59;
-	ALT_x1 = 0; ALT_x2 = 5/* ALTERA = 6 chars */; ALT_y = 0; ALT_inc_x = 1; ALT_inc_y = 1;
-	VGA_text (ALT_x1, ALT_y, text_ALTERA);
-	while (1)
-	{
-		while (!timeout)
-			;	// wait to synchronize with timer 
-
-		/* move the ALTERA text around on the VGA screen */
-		VGA_text (ALT_x1, ALT_y, text_erase);		// erase
-		ALT_x1 += ALT_inc_x; 
-		ALT_x2 += ALT_inc_x; 
-		ALT_y += ALT_inc_y;
-
-		if ( (ALT_y == char_buffer_y) || (ALT_y == 0) )
-			ALT_inc_y = -(ALT_inc_y);
-		if ( (ALT_x2 == char_buffer_x) || (ALT_x1 == 0) )
-			ALT_inc_x = -(ALT_inc_x);
-
-		if ( (ALT_y >= blue_y1 - 1) && (ALT_y <= blue_y2 + 1) )
-		{
-			if ( ((ALT_x1 >= blue_x1 - 1) && (ALT_x1 <= blue_x2 + 1)) ||
-				((ALT_x2 >= blue_x1 - 1) && (ALT_x2 <= blue_x2 + 1)) )
-			{
-				if ( (ALT_y == (blue_y1 - 1)) || (ALT_y == (blue_y2 + 1)) )
-					ALT_inc_y = -(ALT_inc_y);
-				else
-					ALT_inc_x = -(ALT_inc_x);
-			}
-		}
-		VGA_text (ALT_x1, ALT_y, text_ALTERA);
-
-		/* display PS/2 data (from interrupt service routine) on HEX displays */
-		HEX_PS2 (byte1, byte2, byte3);
-		timeout = 0;
-	}
+//	char_buffer_x = 79; char_buffer_y = 59;
+//	ALT_x1 = 0; ALT_x2 = 5/* ALTERA = 6 chars */; ALT_y = 0; ALT_inc_x = 1; ALT_inc_y = 1;
+//	VGA_text (ALT_x1, ALT_y, text_ALTERA);
+	while (1);
 }
 
 /****************************************************************************************
@@ -141,7 +110,7 @@ void VGA_text(int x, int y, char * text_ptr)
   	volatile char * character_buffer = (char *) 0x09000000;	// VGA character buffer
 
 	/* assume that the text string fits on one line */
-//	offset = (y << 7) + x;
+	offset = (y << 7) + x;
 	while ( *(text_ptr) )
 	{
 		*(character_buffer + offset) = *(text_ptr);	// write to the character buffer
@@ -175,32 +144,32 @@ void VGA_box(int x1, int y1, int x2, int y2, short pixel_color)
 /****************************************************************************************
  * Subroutine to show a string of HEX data on the HEX displays
 ****************************************************************************************/
-void HEX_PS2(char b1, char b2, char b3)
-{
-	volatile int * HEX3_HEX0_ptr = (int *) 0x10000020;
-	volatile int * HEX7_HEX4_ptr = (int *) 0x10000030;
+//void HEX_PS2(char b1, char b2, char b3)
+//{
+//	volatile int * HEX3_HEX0_ptr = (int *) 0x10000020;
+//	volatile int * HEX7_HEX4_ptr = (int *) 0x10000030;
 
 	/* SEVEN_SEGMENT_DECODE_TABLE gives the on/off settings for all segments in 
 	 * a single 7-seg display in the DE2 Media Computer, for the hex digits 0 - F */
-	unsigned char	seven_seg_decode_table[] = {	0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 
-		  										0x7F, 0x67, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71 };
-	unsigned char	hex_segs[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-	unsigned int shift_buffer, nibble;
-	unsigned char code;
-	int i;
-
-	shift_buffer = (b1 << 16) | (b2 << 8) | b3;
-	for ( i = 0; i < 6; ++i )
-	{
-		nibble = shift_buffer & 0x0000000F;		// character is in rightmost nibble
-		code = seven_seg_decode_table[nibble];
-		hex_segs[i] = code;
-		shift_buffer = shift_buffer >> 4;
-	}
+//	unsigned char	seven_seg_decode_table[] = {	0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 
+//		  										0x7F, 0x67, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71 };
+//	unsigned char	hex_segs[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+//	unsigned int shift_buffer, nibble;
+//	unsigned char code;
+//	int i;
+//
+//	shift_buffer = (b1 << 16) | (b2 << 8) | b3;
+//	for ( i = 0; i < 6; ++i )
+//	{
+//		nibble = shift_buffer & 0x0000000F;		// character is in rightmost nibble
+//		code = seven_seg_decode_table[nibble];
+//		hex_segs[i] = code;
+//		shift_buffer = shift_buffer >> 4;
+//	}
 	/* drive the hex displays */
-	*(HEX3_HEX0_ptr) = *(int *) (hex_segs);
-	*(HEX7_HEX4_ptr) = *(int *) (hex_segs+4);
-}
+//	*(HEX3_HEX0_ptr) = *(int *) (hex_segs);
+//	*(HEX7_HEX4_ptr) = *(int *) (hex_segs+4);
+//}
 
 
 
