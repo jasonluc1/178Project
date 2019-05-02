febbd8356e51ee684d2c6f5c68a34a02210a676f
alwinv135 <50149720+alwinv135@users.noreply.github.com>
2019-05-01 18:52:08 -0700

    Update pushbutton_ISR.c

--- pushbutton_ISR.c
+++ pushbutton_ISR.c
@@ -1,4 +1,4 @@
-
+void VGA_text2 ( int , int, char *);
 /***************************************************************************************
  * Pushbutton - Interrupt Service Routine                                
  *                                                                          
@@ -16,25 +16,40 @@ void pushbutton_ISR( void )
 	KEY_value = *(KEY_ptr + 3);			// read the pushbutton interrupt register
 	*(KEY_ptr + 3) = 0; 						// Clear the interrupt
 
-	if (KEY_value == 0x8)					// check KEY1
+	if (KEY_value == 0x8)					// check KEY3
 	{
-		*(PS2_ptr + 1) = 0x1; 			/* write to the PS/2 Control register to enable interrupts */
+		*(PS2_ptr) = 0xff;		/*clears the data */ 
+		*(PS2_ptr + 1) = 0x1; 			/* write to the PS/2 Control register to enable interrupts */	
+			char text_erase[80] = "                                                \0";
+			for(int i = 0; i < 240; i++){
+			VGA_text2(2, i, text_erase);
+			}
 	}
-	else if (KEY_value == 0x1)				// check KEY2
+	else if (KEY_value == 0x4)				// check KEY2
 	{
 		*(PS2_ptr + 1) = 0;			/*write to the PS/2 controlregister to disable interrupts */
-		/* the following variables give the size of the pixel buffer */
-		screen_x = 319; screen_y = 239;
-		color = 0x1863;		// a dark grey color
-		VGA_box (0, 0, screen_x, screen_y, color);	// fill the screen with grey
+
 		char text_greet_VGA[20] = "WELCOME!\0";
 		char text_one_VGA[50] = "1) KEY 3 will enable PS/2 to type on the screen\0";
 		char text_two_VGA[30] = "2) KEY 2 will disable PS/2\0";
+		char text_erase[50] = "                                        \0";
+
+	/*Clears the screen before outputting the text on screen so char buffer is empty*/
+		VGA_text2(2, 1, text_erase);
+		VGA_text2(2, 2, text_erase);
+		VGA_text2(2, 3, text_erase);
+
+
+	/*output the menu on the screen*/
+		VGA_text2 (2, 1 , text_greet_VGA);
+		VGA_text2 (2 , 2, text_one_VGA);
+		VGA_text2 (2, 3, text_two_VGA);
+
 	}
-	//else if (KEY_value == 0x8)		//checks if KEY[3] is press
-	//{
-		/*add another function to when the KEY[3] is press*/
-	//}
+	// else if (KEY_value == 0x8)		//checks if KEY[3] is press
+	// {
+	// 	/*add another function to when the KEY[3] is press*/
+	// }
 	return;
 }
 
@@ -43,7 +58,7 @@ void pushbutton_ISR( void )
 /****************************************************************************************
  * Subroutine to send a string of text to the VGA monitor 
 ****************************************************************************************/
-void VGA_text(int x, int y, char * text_ptr)
+void VGA_text2(int x, int y, char * text_ptr)
 {
 	int offset;
   	volatile char * character_buffer = (char *) 0x09000000;	// VGA character buffer
@@ -58,23 +73,23 @@ void VGA_text(int x, int y, char * text_ptr)
 	}
 }
 
-/****************************************************************************************
- * Draw a filled rectangle on the VGA monitor 
-****************************************************************************************/
-void VGA_box(int x1, int y1, int x2, int y2, short pixel_color)
-{
-	int offset, row, col;
-  	volatile short * pixel_buffer = (short *) 0x08000000;	// VGA pixel buffer
+// /****************************************************************************************
+//  * Draw a filled rectangle on the VGA monitor 
+// ****************************************************************************************/
+// void VGA_box(int x1, int y1, int x2, int y2, short pixel_color)
+// {
+// 	int offset, row, col;
+//   	volatile short * pixel_buffer = (short *) 0x08000000;	// VGA pixel buffer
 
-	/* assume that the box coordinates are valid */
-	for (row = y1; row <= y2; row++)
-	{
-		col = x1;
-		while (col <= x2)
-		{
-			offset = (row << 9) + col;
-			*(pixel_buffer + offset) = pixel_color;	// compute halfword address, set pixel
-			++col;
-		}
-	}
-}
+// 	/* assume that the box coordinates are valid */
+// 	for (row = y1; row <= y2; row++)
+// 	{
+// 		col = x1;
+// 		while (col <= x2)
+// 		{
+// 			offset = (row << 9) + col;
+// 			*(pixel_buffer + offset) = pixel_color;	// compute halfword address, set pixel
+// 			++col;
+// 		}
+// 	}
+// }
